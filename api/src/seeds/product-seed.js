1b5024006adc2a1c6d42a198c07693301ab48f8e
camilapanella <ca10_mimi@hotmail.com>
2023-09-14 16:46:34 -0300

    env example

--- api/src/seeds/product-seed.js
+++ api/src/seeds/product-seed.js
@@ -1,57 +1,57 @@
 const types = [
   {
-    name: "JBL Go Essential",
-    description:
+    "name": "JBL Go Essential",
+    "description":
       "Soundcore, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-    price: 200,
-    image_url:
+    "price": 200,
+    "image_url":
       "https://http2.mlstatic.com/D_NQ_NP_912142-MLA51457763140_092022-O.webp",
-    brandId: 1,
+    "brandId": 1
   },
   {
-    name: "Apple Macbook Pro 14",
-    description:
+    "name": "Apple Macbook Pro 14",
+    "description":
       "Soundcore, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-    price: 150,
-    image_url:
+    "price": 150,
+    "image_url":
       "https://static.nb.com.ar/i/nb_APPLE-MACBOOK-PRO-14-M1-PRO-CHIP-16GB-512GB-SSD-SILVER_ver_a1f627781115284cebfa693795adc22b.jpeg",
-    brandId: 2,
+    "brandId": 2
   },
   {
-    name: "Mouse Bluetooth POP",
-    description:
+    "name": "Mouse Bluetooth POP",
+    "description":
       "Soundcore, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-    price: 200,
-    image_url:
+    "price": 200,
+    "image_url":
       "https://logg.api.cygnus.market/static/logg/Global/Mouse%20Bluetooth%20Logitech%20POP%20Heartbreaker%20Bot%C3%B3n%20de%20Emojis%20comp.%20Logi%20Bolt%20Rosa/Rojo/8a9edb0b546942a38e4d585aae44ee25.webp",
-    brandId: 3,
+    "brandId": 3
   },
   {
-    name: "Galaxy A23",
-    description:
+    "name": "Galaxy A23",
+    "description":
       "Soundcore, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-    price: 200,
-    image_url:
+    "price": 200,
+    "image_url":
       "https://images.samsung.com/is/image/samsung/p6pim/ar/sm-a235mlbearo/gallery/ar-galaxy-a23-sm-a235-sm-a235mlbearo-532206854?$650_519_PNG$",
-    brandId: 4,
+    "brandId": 4
   },
   {
-    name: "Moto G13 SE",
-    description:
+    "name": "Moto G13 SE",
+    "description":
       "Soundcore, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-    price: 200,
-    image_url:
+    "price": 200,
+    "image_url":
       "https://armoto.vtexassets.com/arquivos/ids/164176-800-auto?v=638180621631900000&width=800&height=auto&aspect=true",
-    brandId: 5,
+    "brandId": 5
   },
   {
-    name: "Huawei Nova 9",
-    description:
+    "name": "Huawei Nova 9",
+    "description":
       "Soundcore, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-    price: 200,
-    image_url:
+    "price": 200,
+    "image_url":
       "https://ar.celulares.com/fotos/huawei-nova-9-93637-g-alt.jpg",
-    brandId: 6,
+    "brandId": 6
   },
 ];
 
