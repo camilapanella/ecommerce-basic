2cb4ed584209606acfb0390c30ac615004c7dd51
camilapanella <ca10_mimi@hotmail.com>
2023-09-14 17:01:15 -0300

    post product response

--- api/src/controllers/postProduct.js
+++ api/src/controllers/postProduct.js
@@ -4,7 +4,7 @@ const { Product } = require("../db.js");
 module.exports = async (req, res) => {
   try {
     const { name } = req.body;
-    if (!name) return response(res, 400, "Mandatory data missing");
+    if (!name) return response(res, 404, "Mandatory data missing");
     const newProduct = await Product.create(req.body);
     response(res, 200, newProduct);
   } catch (error) {
